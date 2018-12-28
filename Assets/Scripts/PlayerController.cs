23f1d7be6299c33eed9f423d3a9bd9377fc21c85
Xwilarg <xwilarg@yahoo.fr>
2018-12-28 15:19:17 +0100

    Add NPC detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,10 +10,13 @@ public class PlayerController : MonoBehaviour
     private const float jumpMinDist = .5f;
     private const float jumpForce = 5f;
 
+    private NPC NpcInterraction;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        NpcInterraction = null;
     }
 
     private void Update()
@@ -28,6 +31,29 @@ public class PlayerController : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, jumpMinDist, 1 << 8);
             if (hit.distance > float.Epsilon)
                 rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("NPC"))
+        {
+            NpcInterraction = collision.GetComponent<NPC>();
+            collision.transform.GetChild(0).gameObject.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("NPC"))
+        {
+            NpcInterraction = null;
+            collision.transform.GetChild(0).gameObject.SetActive(false);
         }
     }
 }
