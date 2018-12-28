66d6766d75ff6410841a53ad1b895f07c796ddae
Nina <ninarachaebuie@gmail.com>
2018-12-28 17:08:03 -0600

    Set up template level heirarchy, started world

---
+++ Assets/Scripts/save system version/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+ 
+    public static GameManager instance;
+
+    void Start()
+    { 
+      if (instance == null) {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+      } else {
+        Destroy(this.gameObject);
+      }
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
