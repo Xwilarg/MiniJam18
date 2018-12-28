7cbeffc4cc29e9f60b280ef41456add5621837ad
Xwilarg <xwilarg@yahoo.fr>
2018-12-28 14:09:22 +0100

    Add player controller

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform toFollow;
+
+    private void Update()
+    {
+        float x = toFollow.position.x;
+        if (x < 0f)
+            x = 0f;
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+}
