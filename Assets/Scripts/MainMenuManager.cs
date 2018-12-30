5bcd2cfbe3f9ea1e3bd7f1b16aa6806b34fb893c
Nina <ninarachaebuie@gmail.com>
2018-12-30 13:14:13 -0600

    Added main menu screen

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject main;
+    public GameObject controls;
+    public GameObject credits;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("World");
+    }
+
+    public void Controls()
+    {
+        controls.SetActive(true);
+        main.SetActive(false);
+    }
+
+    public void Credits()
+    {
+        credits.SetActive(true);
+        main.SetActive(false);
+    }
+
+    public void Back()
+    {
+        controls.SetActive(false);
+        credits.SetActive(false);
+        main.SetActive(true);
+    }
+}
